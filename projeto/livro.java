1697d29001803e542cc8428b99dd45bf2fbbea05
MarlonOrtiz23 <marlon.ortiz19@gmail.com>
2016-09-08 22:10:42 -0300

    Create livro.java

---
+++ projeto/livro.java
@@ -0,0 +1,63 @@
+package negocio;
+
+public class Livro {
+    private String autor;
+    private String titulo;
+    private String capa;
+    private double avaliacao;
+    private double valor;
+
+    public Livro(String autor, String titulo, String capa, double avaliacao, double valor) {
+        this.autor = autor;
+        this.titulo = titulo;
+        this.capa = capa;
+        this.avaliacao = avaliacao;
+        this.valor = valor;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getCapa() {
+        return capa;
+    }
+
+    public double getAvaliacao() {
+        return avaliacao;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public void setCapa(String capa) {
+        this.capa = capa;
+    }
+
+    public void setAvaliacao(double avaliacao) {
+        this.avaliacao = avaliacao;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+    
+
+    @Override
+    public String toString() {
+        return titulo + "," + autor + "," + "avaliação(" + avaliacao + ")," + capa + "," + valor;
+    }
+}
